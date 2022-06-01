4296a670f0865685f0ccffb420b180a23092a8ef
Zephelion <russellnumo@outlook.com>
2022-06-01 19:28:08 +0200

    states and cleaned code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from "react"
 
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id:1,
+            text: 'Tandarts afspraak',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true, 
+        },
+        {
+            id:2,
+            text: 'Vibing somewhere',
+            day: 'Feb 6th at 3:30pm',
+            reminder: true, 
+        },
+        {
+            id:3,
+            text: 'Contemplating some stuff',
+            day: 'Feb 5th at 5:00pm',
+            reminder: false, 
+        }
+    ]
+  )
+
+  const deleteTask = (id) => {
+    console.log("delete", id)
+  }
+
+
   return (
-    <div className="App">
+    <div className="container">
       <Header/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = (props) => {
+
+  const onClick = () =>{
+    console.log("on click");
+  }
+
   return (
-    <header>
-        <h1>React Application</h1>
+    <header className='header'>
+        <h1>{props.title}</h1>
+        <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
 }
+Header.defaultProps = {
+  title: 'React Application'
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+// const HeadingStyle = {
+//   color: 'red',
+//   backgroundColor:'black'
+// }
 
 export default Header
